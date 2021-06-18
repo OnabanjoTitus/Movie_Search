941844e9c689351cfadc607888d4d27977081f8e
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-18 18:18:42 +0100

    First commit

---
+++ src/test/java/com/moviesSearch/MoviesSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
